fdb7c620a419fb9981226834ffc4f81bf86fa577
Luke Connor <lukeconnor.dev@gmail.com>
2020-12-30 14:19:16 -0800

    added python script to spin a model by adjusting rotation over multiple executions

---
+++ spin.py
@@ -0,0 +1,24 @@
+import subprocess, os, shutil, time
+from subprocess import Popen
+from shutil import copyfile
+
+print("Spin")
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+exePath = "/out/build/x64-Release/"
+exeName = "raytracer.exe"
+
+angle = 1
+maxAngle = 180
+
+#print ("Path: ", exePath)
+
+
+while angle < maxAngle:
+  print("ANGLE:", angle)
+  proc = subprocess.Popen([dir_path+exePath+exeName, "-rotate", str(angle)], cwd=dir_path+exePath);
+  proc.wait();
+  copyfile(dir_path + exePath + "output.tga", dir_path + exePath + "frames/" + str(angle) + ".tga")
+  # cool down for a few seconds
+  time.sleep(2)
+  angle += 15
